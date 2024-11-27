dc83031d007c2e0520934a041d7bfb631cdf4c4e
VaclavHrbek <st43190@student.upce.cz>
2024-11-27 21:52:16 +0000

    Add Button component.

---
+++ vaclav-app/src/components/side-menu/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    label: String,
+})
+
+</script>
+
+<template>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        {{ label }}
+    </button>
+</template>
