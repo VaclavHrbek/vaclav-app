016b01f51ea93bec0818af7157867e106529bb35
Vaclav Hrbek <st43190@upce.cz>
2024-11-28 09:37:17 +0000

    Add ToDo and Item components for task management

---
+++ vaclav-app/src/components/to-do-app/Item.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+    label: String,
+})
+
+</script>
+
+<template>
+    <div>
+        <span>{{ label }}</span>
+    </div>
+</template>
---
+++ vaclav-app/src/components/to-do-app/ToDo.vue
@@ -0,0 +1,14 @@
+<script setup>
+import item from './Item.vue'
+
+</script>
+
+<template>
+    <div class="bg-green-200 flex-auto">
+        <ul class="list-disc list-inside">
+            <item label="Task 1"/>
+            <item label="Task 2"/>
+            <item label="Task 3"/>
+        </ul>
+    </div>
+</template>
