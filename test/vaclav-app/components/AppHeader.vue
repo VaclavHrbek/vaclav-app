5715f3d4467fc46943aa1dd6f7724e8ba5ef80e0
VaclavHrbek <st43190@student.upce.cz>
2024-12-14 22:27:52 +0000

    Update the layout of the app.

---
+++ test/vaclav-app/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+            <div class="bg-gray-800 h-20 items-center flex justify-center">
+                <div class="bg-gray-600 max-w-7xl h-16 flex">
+                    <div class="bg-gray-400 flex-1 h-10">
+                    </div>
+                </div>
+            </div>
+    </header>
+</template>
